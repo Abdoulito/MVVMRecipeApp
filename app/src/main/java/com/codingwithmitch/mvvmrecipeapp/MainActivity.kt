340cdfbfda40515c7599dbd230fc3fe4353964d1
Abdoulito <abdoul-moumouni.alou-abdourahamane.1@ens.etsmtl.ca>
2022-10-29 22:48:08 -0400

    NavGraph setup successfully

--- app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.codingwithmitch.mvvmrecipeapp
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 
 class MainActivity : AppCompatActivity(){
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      setContentView(R.layout.activity_main)
+/*    setContent {
+        Column() {
+            Button(onClick = { *//*TODO*//* }) {
+                Text(text = "THIS BUTTON IS COOL")
+            }
+        }
+    } */
+     setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/codingwithmitch/mvvmrecipeapp/RecipeFragment.kt
@@ -0,0 +1,24 @@
+package com.codingwithmitch.mvvmrecipeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class RecipeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "RECIPE FRAGMENT")
+            }
+        }
+    }
+}
